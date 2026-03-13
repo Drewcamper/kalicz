d85a45bdf73474195712ddc2042d74c9638aead4
Andras korosi <89639857+Drewcamper@users.noreply.github.com>
2026-03-13 16:10:17 +0100

    fix: styling for slideshow

--- src/home/slideshow/styles.js
+++ src/home/slideshow/styles.js
@@ -1,13 +1,13 @@
 export const styles = {
   container: {
-    width: '100%',
+    // width: '100%',
     height: 'calc(97vh - 59px)',
     overflow: 'hidden',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     position: 'relative',
-    padding: '48px 0px',
-    margin: '0px 20px',
+    padding: '48px 20px',
+    // margin: '0px 20px',
   },
 };
